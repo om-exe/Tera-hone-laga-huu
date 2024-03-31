b6c064bae5967abd52274fb961924019347265cc
gargik06 <152643823+gargik06@users.noreply.github.com>
2024-03-31 15:35:27 +0530

    Update prac4(b).R

--- SM/prac4(b).R
+++ SM/prac4(b).R
@@ -1,7 +1,7 @@
 total =1:6
 prob=rep(1/6,6)
-mean =(total)
-variance =((total-mean)*2*prob)
-SD=sqrt(variance)
-cat("Standard variance is ",SD,"\n")
-cat("variance is ",variance,"\n")
+mean =sum(total*prob)
+variance =sum((total-mean)^2*prob)
+sd=sqrt(variance)
+cat("variance is=",variance,"\n")
+cat("standard deviation=",sd,"\n")
