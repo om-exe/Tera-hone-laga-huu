cd2ed09f878f836306cb7fb0c8e0da6179777f19
gargik06 <152643823+gargik06@users.noreply.github.com>
2024-03-31 15:49:08 +0530

    Update prac5.R

--- SM/prac5.R
+++ SM/prac5.R
@@ -1,3 +1,4 @@
+standard probability distribution
 n=5
 p=1/2
 q=(1-p)
@@ -9,7 +10,7 @@ cat("variance=",variance,"\n")
 cat("standard deviation=",sd,"\n")
 
 
-
+probability of normal distribution
 x_values=2.3
 prob=pnorm(x_values)
 cat("prob of 2.3 in normal distribution=",prob)
