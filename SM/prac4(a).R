97844b70417bb44cf36ef4fd22091989cc00f4f3
gargik06 <152643823+gargik06@users.noreply.github.com>
2024-03-31 16:10:58 +0530

    Update prac4(a).R

--- SM/prac4(a).R
+++ SM/prac4(a).R
@@ -1,10 +1,11 @@
+mean of discrete and continuous prob distribution
 out=1:6;
 pro=rep(1/6,6);
 mean=sum(out*pro);
-cat(mean,"\n")
+cat("mean is","\n")
 
 
-
+mean of continuous prob distribution (standard normal)
 library(stats);
 mean_conn=mean(rnorm(500));
-cat(mean_conn);
+cat(mean_conn)
