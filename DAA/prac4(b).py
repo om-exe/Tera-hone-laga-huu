0e9f8f722c674cfc79cf6661c2e82ce22113fecc
om-exe <ajinkya9876543@gmail.com>
2024-04-03 05:42:17 +0530

    Add files via upload

---
+++ DAA/prac4(b).py
@@ -0,0 +1,13 @@
+def BinarySearch(array,target):
+    # swapped = False
+    right,left=0,len(array)
+    while right<=left:
+        mid = right+left//2
+    if target==array(mid):
+        return mid
+    elif array[mid]<target:
+         left = mid+1
+    else:
+        right = mid+1
+a = [12,56,4,57,95,5,28]
+print(BinarySearch(a,4))
