1cd8fc02a53ec41762f6a47dc63cf635275d4588
gargik06 <152643823+gargik06@users.noreply.github.com>
2024-04-04 12:41:49 +0530

    Update prac1_a1.cpp

--- C++ Practical/Practical 1/prac1_a1.cpp
+++ C++ Practical/Practical 1/prac1_a1.cpp
@@ -1,8 +1,10 @@
-#include<iostream>
-using namespace std;
-int main(){
-    int a=23,b=5,c;
-    c=a+b;
-    cout<<"The result is "<<c;
-    return 0;
+#include<iostream.h>
+#include<conio.h>
+void main(){
+clrscr();
+int a=23,b=5,c;
+c=a+b;   
+cout<<"The add of a&b is:";
+cout<<c;
+getch();
 }
