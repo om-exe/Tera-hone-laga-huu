9554bc25c3835e73b84ced62b05ece958de8a299
om-exe <ajinkya9876543@gmail.com>
2024-08-02 22:02:47 +0530

    Delete C++ Practical directory

--- C++ Practical/Practical 1/prac1_a5.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-using namespace std;
-int main(){
-    int a=25,b=15,c;
-    c=a%b;
-    cout<<"The result is "<<c;
-    return 0;
-}
--- C++ Practical/Practical 2/prac2_g.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-int main(){
-    char a[50];
-    cout<<"Enter a string ";
-    gets(a);
-    cout<<"The entered string is : "<<a;   
-    return 0;
-}
